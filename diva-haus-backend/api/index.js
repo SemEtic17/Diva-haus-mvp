d85ccc4d8a90cbab244602950bdefe672b07d1f0
SemEtic17 <semetic17@gmail.com>
2025-12-05 19:19:24 +0300

    configure cors for frontend

--- diva-haus-backend/api/index.js
+++ diva-haus-backend/api/index.js
@@ -10,7 +10,10 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json()); // Body parser for JSON requests
 
 // Connect to MongoDB
